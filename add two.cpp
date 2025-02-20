187132389d25678ad3082dcb4494ce821692c08f
Soms2002 <nsomesh025@gmail.com>
2025-02-20 10:37:49 +0530

    Add files via upload

---
+++ add two.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      int a,b,c;
+      cout<<"Enter fist Number";
+      cin>>a;
+      cout<<"Enter second Number";
+      cin>>b;
+      c=a+b;
+      cout<< "sum=" <<c;
+      return 0;
+
+}
